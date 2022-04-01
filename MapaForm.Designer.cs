0a9b28fcf229a64294de6d7237f556ab0377a83e
QatarAndora <dimitrije.golubovic@borastankovic.edu.rs>
2022-04-01 20:36:55 +0200

    dd

--- MapaForm.Designer.cs
+++ MapaForm.Designer.cs
@@ -94,7 +94,7 @@ namespace Hackathon_Project_GUI
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Pretraži";
+            this.button1.Text = "Pretraga";
             this.toolTip2.SetToolTip(this.button1, "Nađite parkinge u vašoj okolini.");
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
