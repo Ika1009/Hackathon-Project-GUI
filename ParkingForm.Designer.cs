10a1d5ce1fc9cbe174c2f15362fcc936cfb66057
Milos Mitrovic <milosmitrovic005@gmail.com>
2022-04-01 19:10:04 +0200

    parking uradjen - milos

--- ParkingForm.Designer.cs
+++ ParkingForm.Designer.cs
@@ -29,12 +29,134 @@ namespace Hackathon_Project_GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pretrazi = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Ulica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Brojpmmesta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Zona = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Napomena = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.pretrazi);
+            this.panel1.Location = new System.Drawing.Point(3, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(185, 521);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "   Izaberite opstinu";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 263);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // pretrazi
+            // 
+            this.pretrazi.Location = new System.Drawing.Point(18, 352);
+            this.pretrazi.Name = "pretrazi";
+            this.pretrazi.Size = new System.Drawing.Size(143, 49);
+            this.pretrazi.TabIndex = 2;
+            this.pretrazi.Text = "Pretrazi";
+            this.pretrazi.UseVisualStyleBackColor = true;
+            this.pretrazi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ulica,
+            this.Brojpmmesta,
+            this.Zona,
+            this.Napomena});
+            this.dataGridView1.Location = new System.Drawing.Point(194, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(781, 521);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Ulica
+            // 
+            this.Ulica.HeaderText = "Pozicija - ulica";
+            this.Ulica.MinimumWidth = 6;
+            this.Ulica.Name = "Ulica";
+            this.Ulica.ReadOnly = true;
+            this.Ulica.Width = 225;
+            // 
+            // Brojpmmesta
+            // 
+            this.Brojpmmesta.HeaderText = "Broj pm mesta";
+            this.Brojpmmesta.MinimumWidth = 6;
+            this.Brojpmmesta.Name = "Brojpmmesta";
+            this.Brojpmmesta.ReadOnly = true;
+            this.Brojpmmesta.Width = 75;
+            // 
+            // Zona
+            // 
+            this.Zona.HeaderText = "Zona";
+            this.Zona.MinimumWidth = 6;
+            this.Zona.Name = "Zona";
+            this.Zona.ReadOnly = true;
+            this.Zona.Width = 75;
+            // 
+            // Napomena
+            // 
+            this.Napomena.HeaderText = "Napomena";
+            this.Napomena.MinimumWidth = 6;
+            this.Napomena.Name = "Napomena";
+            this.Napomena.ReadOnly = true;
+            this.Napomena.Width = 375;
+            // 
+            // ParkingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(980, 578);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "ParkingForm";
             this.Text = "ParkingForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button pretrazi;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ulica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Brojpmmesta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Zona;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Napomena;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
