83460c6139005b7ed1d983f3346399a24a120ad1
miamihotlinevol3 <nikola.andjelkovic.nix@gmail.com>
2022-04-02 13:35:41 +0200

    drqwerfr4trt

--- DodavanjeVolonteraForm.Designer.cs
+++ DodavanjeVolonteraForm.Designer.cs
@@ -42,11 +42,14 @@
             this.gradtextBox = new System.Windows.Forms.TextBox();
             this.opstinaTextBox = new System.Windows.Forms.TextBox();
             this.polTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // imeTextBox
             // 
-            this.imeTextBox.Location = new System.Drawing.Point(148, 59);
+            this.imeTextBox.Location = new System.Drawing.Point(162, 80);
             this.imeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.imeTextBox.Name = "imeTextBox";
             this.imeTextBox.Size = new System.Drawing.Size(110, 23);
@@ -54,7 +57,7 @@
             // 
             // prezimeTextBox
             // 
-            this.prezimeTextBox.Location = new System.Drawing.Point(148, 103);
+            this.prezimeTextBox.Location = new System.Drawing.Point(162, 110);
             this.prezimeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.prezimeTextBox.Name = "prezimeTextBox";
             this.prezimeTextBox.Size = new System.Drawing.Size(110, 23);
@@ -62,7 +65,7 @@
             // 
             // godineTextBox
             // 
-            this.godineTextBox.Location = new System.Drawing.Point(148, 166);
+            this.godineTextBox.Location = new System.Drawing.Point(162, 170);
             this.godineTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.godineTextBox.Name = "godineTextBox";
             this.godineTextBox.Size = new System.Drawing.Size(110, 23);
@@ -70,18 +73,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(162, 341);
+            this.button1.BackColor = System.Drawing.Color.LawnGreen;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(139, 334);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(82, 22);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Potvrdi";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // brojTelefonaTextBox
             // 
-            this.brojTelefonaTextBox.Location = new System.Drawing.Point(148, 204);
+            this.brojTelefonaTextBox.Location = new System.Drawing.Point(162, 200);
             this.brojTelefonaTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.brojTelefonaTextBox.Name = "brojTelefonaTextBox";
             this.brojTelefonaTextBox.Size = new System.Drawing.Size(110, 23);
@@ -90,7 +95,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(183, 111);
+            this.label1.Location = new System.Drawing.Point(197, 132);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 15);
             this.label1.TabIndex = 6;
@@ -98,7 +103,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 62);
+            this.label2.Location = new System.Drawing.Point(81, 80);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(27, 15);
             this.label2.TabIndex = 7;
@@ -107,7 +112,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 111);
+            this.label3.Location = new System.Drawing.Point(81, 113);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 15);
             this.label3.TabIndex = 8;
@@ -116,7 +121,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(55, 174);
+            this.label4.Location = new System.Drawing.Point(81, 173);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(45, 15);
             this.label4.TabIndex = 9;
@@ -125,7 +130,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(51, 204);
+            this.label5.Location = new System.Drawing.Point(81, 203);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(75, 15);
             this.label5.TabIndex = 10;
@@ -147,7 +152,7 @@
             // 
             // gradtextBox
             // 
-            this.gradtextBox.Location = new System.Drawing.Point(148, 242);
+            this.gradtextBox.Location = new System.Drawing.Point(162, 230);
             this.gradtextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gradtextBox.Name = "gradtextBox";
             this.gradtextBox.Size = new System.Drawing.Size(110, 23);
@@ -156,7 +161,7 @@
             // 
             // opstinaTextBox
             // 
-            this.opstinaTextBox.Location = new System.Drawing.Point(148, 280);
+            this.opstinaTextBox.Location = new System.Drawing.Point(162, 260);
             this.opstinaTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.opstinaTextBox.Name = "opstinaTextBox";
             this.opstinaTextBox.Size = new System.Drawing.Size(110, 23);
@@ -164,17 +169,47 @@
             // 
             // polTextBox
             // 
-            this.polTextBox.Location = new System.Drawing.Point(148, 139);
+            this.polTextBox.Location = new System.Drawing.Point(162, 140);
             this.polTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.polTextBox.Name = "polTextBox";
             this.polTextBox.Size = new System.Drawing.Size(110, 23);
             this.polTextBox.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(81, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Pol";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 233);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Opština";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(81, 263);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 15);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Grad";
             // 
             // DodavanjeVolonteraForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(365, 383);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.polTextBox);
             this.Controls.Add(this.opstinaTextBox);
             this.Controls.Add(this.gradtextBox);
@@ -213,6 +248,8 @@
         private System.Windows.Forms.TextBox gradtextBox;
         private System.Windows.Forms.TextBox opstinaTextBox;
         private System.Windows.Forms.TextBox polTextBox;
-
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
